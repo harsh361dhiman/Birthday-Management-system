adcb2611594b77ceed26e2eb9d4b22040e6f46fa
Harsh Dhiman <harsh361dhiman@gmail.com>
2026-08-29 12:48:56 +0530

    Refactor server.js for improved email scheduling

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.listen(PORT, () => {
 
 // ================= AUTOMATIC BIRTHDAY EMAIL =================
 
-cron.schedule("40 12 * * *", async () => {
+cron.schedule("50 12 * * *", async () => {
 
     try {
 
