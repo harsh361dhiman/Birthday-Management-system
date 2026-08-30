2615c52d0d49916d3dc63f30c49243f590db3f7b
Harsh Dhiman <harsh361dhiman@gmail.com>
2026-08-30 16:05:37 +0530

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -2,13 +2,14 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    family: 4   // 🆕 Ye line add ki — IPv4 force karne ke liye
+    family: 4 ,  // 🆕 Ye line add ki — IPv4 force karne ke liye
+    connectionTimeout: 10000 
 });
 
 const sendBirthdayEmail = async (student) => {
