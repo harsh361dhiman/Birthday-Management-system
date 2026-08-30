8d67b97319c9a50fda8387cfc6fda99279d3b4a0
Harsh Dhiman <harsh361dhiman@gmail.com>
2026-08-30 13:09:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.listen(PORT, () => {
 
 // ================= AUTOMATIC BIRTHDAY EMAIL =================
 
-cron.schedule("50 12 * * *", async () => {
+cron.schedule("20 13 * * *", async () => {
 
     try {
 
